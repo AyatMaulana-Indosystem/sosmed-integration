751c310d26ee29ab1b7087620cf27aeeefa7e37b
AyatMaulana <Ayatmaulana.indosystem@gmail.com>
2016-11-01 17:47:59 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Social Media Integration</title>
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" type="text/css" href="../assets/css/vendor.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/flat-admin.css">
+
+  <!-- Theme -->
+  <link rel="stylesheet" type="text/css" href="../assets/css/theme/blue-sky.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/theme/blue.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/theme/red.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/theme/yellow.css">
+
+</head>
+<body>
+  <div class="app app-default">
+
+<div class="app-container app-login">
+  <div class="flex-center">
+    <div class="app-header"></div>
+    <div class="app-body">
+      <div class="loader-container text-center">
+          <div class="icon">
+            <div class="sk-folding-cube">
+                <div class="sk-cube1 sk-cube"></div>
+                <div class="sk-cube2 sk-cube"></div>
+                <div class="sk-cube4 sk-cube"></div>
+                <div class="sk-cube3 sk-cube"></div>
+              </div>
+            </div>
+          <div class="title">Logging in...</div>
+      </div>
+      <div class="app-block">
+        <div class="app-right-section">
+          <div class="app-brand"><span class="highlight">Social Media</span> Integration</div>
+          <div class="app-info">
+
+            <button type="button" class="btn btn-default btn-sm btn-social __instagram">
+              <div class="info">
+                <i class="icon fa fa-instagram" aria-hidden="true"></i>
+                <span class="title">Instagram</span>
+              </div>
+            </button>
+          
+          <button type="button" class="btn btn-default btn-sm btn-social __facebook">
+              <div class="info">
+                <i class="icon fa fa-facebook-official" aria-hidden="true"></i>
+                <span class="title">Facebook</span>
+              </div>
+            </button>
+            <button type="button" class="btn btn-default btn-sm btn-social __twitter">
+              <div class="info">
+                <i class="icon fa fa-twitter" aria-hidden="true"></i>
+                <span class="title">Twitter</span>
+              </div>
+            </button>
+
+
+            {{-- <ul class="list">
+              <li>
+                <div class="icon">
+                  <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+                </div>
+                <div class="title">Increase <b>Productivity</b></div>
+              </li>
+              <li>
+                <div class="icon">
+                  <i class="fa fa-cubes" aria-hidden="true"></i>
+                </div>
+                <div class="title">Lot of <b>Components</b></div>
+              </li>
+              <li>
+                <div class="icon">
+                  <i class="fa fa-usd" aria-hidden="true"></i>
+                </div>
+                <div class="title">Forever <b>Free</b></div>
+              </li>
+            </ul> --}}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="app-footer">
+    </div>
+  </div>
+</div>
+
+  </div>
+  
+  <script type="text/javascript" src="../assets/js/vendor.js"></script>
+  <script type="text/javascript" src="../assets/js/app.js"></script>
+
+</body>
+</html>
